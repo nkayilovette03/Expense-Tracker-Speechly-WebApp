fd5588a808fdef12fad6da9d9da3e24dc8a3daf1
Nkayi Lovette <nkayilovette03@gmail.com>
2021-05-03 15:47:28 +0100

    I just finished with all crude functionality and heading to intergrating speechly for voice assistance that is going to toughgit add .

---
+++ src/components/Details/Details.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
+import { Doughnut } from "react-chartjs-2";
+
+import useStyles from "./styles";
+import useTrasactions from "../../useTransactions";
+
+const Details = ({ title }) => {
+  const classes = useStyles();
+
+  const { total, chartData } = useTrasactions(title);
+
+  return (
+    <Card className={title === "Income" ? classes.income : classes.expense}>
+      <CardHeader title={title} />
+      <CardContent>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Details;
